e0cddc392e4680b5899ee2abc8b36ce24f769ba6
Viktor Chizhikov <vchizhikov@zuzex.com>
2022-05-05 16:41:09 +0300

    configure project

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,52 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
-  </div>
+  <v-app>
+    <v-main class="app">
+      <router-view v-slot="{ Component }">
+        <component :is="Component" />
+      </router-view>
+    </v-main>
+  </v-app>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+@import url("https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100&family=Roboto:wght@300;400;500&display=swap");
+
+:root {
+  --main-white: #fcfcfc;
+  --main-dark: #212121;
 }
 
-#nav {
-  padding: 30px;
+.app {
+  background-color: rgb(250, 250, 250);
+}
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+@media (min-width: 1264px) {
+  .v-container {
+    max-width: 1390px;
   }
+  .v-container--fluid {
+    max-width: 100%;
+  }
+}
+
+.underline {
+  background-color: #00695c;
+  height: 1px;
+  width: 0px;
+  transition: 0.2s;
+}
+
+*:hover > .underline {
+  width: 100%;
+}
+
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.1s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
 }
 </style>
---
+++ src/views/HomeView/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script src="./HomeView.ts" lang="ts">
+</script>
+
+<style src="./HomeView.scss" lang="scss" scoped>
+</style>
