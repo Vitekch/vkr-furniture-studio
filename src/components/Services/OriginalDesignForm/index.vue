42a983515cb2c60a98a7a8701f23b4ed8e491613
Viktor Chizhikov <vchizhikov@zuzex.com>
2022-05-13 16:10:10 +0300

    feat: original design description

--- src/components/Services/OriginalDesignForm/index.vue
+++ src/components/Services/OriginalDesignForm/index.vue
@@ -1,8 +1,19 @@
 <template>
   <v-container class="d-flex justify-center">
-    <v-card max-width="600" class="mt-10 pt-4">
+    <v-card max-width="600" class="mt-10">
       <v-form ref="contactForm" @submit.prevent="submitHandler">
         <v-container>
+          <v-row>
+            <v-col class="text-body-2 text-justify">
+              Обратившись к нам, вы сможете создать совершенно уникальную
+              обстановку для вашей кухни или любого другого помещения. В этом
+              вам помогут консультации наших высококлассных дизайнеров. Каждый
+              из них настоящий специалист своего дела, имеющий профильное
+              образование. И посетив наш салон, Вы будете общаться не с
+              продавцами-консультантами, а с квалифицированными дизайнерами.
+            </v-col>
+          </v-row>
+          <v-divider class="my-5"></v-divider>
           <v-row>
             <v-col md="4" cols="12">
               <v-text-field
